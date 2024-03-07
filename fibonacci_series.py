c3f2c3435f309286ae62bd49025c45cc0c9f6310
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-03-06 20:29:01 -0500

    Update fibonacci_series.py

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,1 +1,11 @@
+# This is the code for the fibonacci_series cloud function.
+def fibonacci_sum(request):
+    fibonacci_sequence = [1, 1]
+    while True:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        if next_number >= 100:
+            break
+        fibonacci_sequence.append(next_number)
+    return {"sum": sum(fibonacci_sequence)}
+
 
